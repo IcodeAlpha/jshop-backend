130822bafed65d86730a5bfca8028c896315108b
IcodeAlpha <alphacode4@outlook.com>
2025-04-26 10:41:00 +0300

    mpesa authorization

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+dotenv.config()
+
 const app = express()
 
 // initiate -stk
 // callback endpoint
 
 app.post("/initiate", (req, res) => {
-    console.log("initiate endpoint hit")
+    try {
+
+        //phonenumber, amount, product_name
+        //254769009184
+        //250
+        //"string"
+
+        const {phoneNumber, amount, productName} = req.body;
+
+        // initiateSTK push
+    } catch (error) {
+        
+    }
 })
 
 app.listen(3000, () => console.log('Server started sucessfully!'))
