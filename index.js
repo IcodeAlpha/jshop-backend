691de12679e854a88f5e59c88e05ee410160a580
IcodeAlpha <alphacode4@outlook.com>
2025-04-28 21:33:18 +0300

    callback endpoint

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { getAccessToken } from './lib/auth.js'
+import { stkPush } from './lib/stkPush.js'
 
 dotenv.config()
 
 const app = express()
+app.use(express.json())
 
 // initiate -stk
-// callback endpoint
-
 app.post("/initiate", async (req, res) => {
     try {
 
-        //phonenumber, amount, product_name
-        //254769009184
-        //250
-        //"string"
-
-        // const {phoneNumber, amount, productName} = req.body;
+        const {phoneNumber, amount, productName} = req.body;
+        // Database logic to store the transaction details
+        // TABLE: Transaction
+        // Status be pending
 
         // initiateSTK push
         // 1. Get access token
         const accessToken = await getAccessToken()
 
+        // 2. Initiate STK push
+        const initiateStkResponse = await stkPush(accessToken, phoneNumber, amount, productName)
 
 
         res.json({
             success: true,
-            accessToken
+            initiateStkResponse
         })
 
 
@@ -39,6 +39,28 @@ app.post("/initiate", async (req, res) => {
             error: error.message || "Failed to get access token"
         })
         
+    }
+})
+
+// callback endpoint
+app.post("/callback", async (req, res) => {
+    try {
+
+        const stkCallbackData = req.body.Body;
+
+        let status = null
+        if(stkCallbackData.ResultCode === 0) {
+           status = "SUCCESS" 
+        } else {
+            status = "FAILED"
+        }
+
+        // Database Logic to update the transaction status.
+
+        res.json({status, stkCallbackData})
+        
+    } catch (error) {
+        res.status(500).json({error: 'something went wrong'})
     }
 })
 
